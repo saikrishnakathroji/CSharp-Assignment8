e4b53a98dcc46022f194661afc2860a1584818e3
saikrishnakathroji <89527892+saikrishnakathroji@users.noreply.github.com>
2022-04-18 01:30:44 +0530

    Add files via upload

---
+++ BankingUsingReflection.cs
@@ -0,0 +1,63 @@
+﻿/*2b)	Design a simple Console Application for testing refection concept of .NET Framework:
+	Define SoftwareAttribute as Custom Attribute
+	Write two classes HDFCAccount, ICICIAccount Apply
+SoftwareAttribute to these classes.
+	Write Test class which will read attributes applied on each classes using reflection technique.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Project8
+{
+    public class BankingUsingReflection
+    {
+        static void Main()
+        {
+
+            var types = from t in Assembly.GetExecutingAssembly().GetTypes()
+                        where t.GetCustomAttributes<ClientAttribute>().Count() > 0
+                        select t;
+
+            foreach (var type in types)
+            {
+                /*Name Of the class*/
+                Console.WriteLine("This is the {0} Bank Class : ",type.Name);
+
+                /*Getting Properties List */
+                foreach (var t in type.GetProperties())
+                {
+                    Console.WriteLine("These  are properties present in the {1} classs : {0}", t.Name,type.Name);
+                }
+            }
+        }
+    }
+    [AttributeUsage(AttributeTargets.Class)]
+
+    /*CustomAttribute*/
+    public class CustomAttribute : Attribute
+    {
+        private string ProjectName { get; set; }
+        private string Descripition { get; set; }
+    }
+    [Custom]
+    public class Hdfc
+    {
+
+        public string Hdfc_Id { get; set; }
+        public string Hdfc_AccountNumber { get; set; }
+        public string Location { get; set; }
+        public string Hdfc_AccountBalance { get; set; }
+    }
+    [Custom]
+    public class Icici
+    {
+        public string Icici_Id { get; set; }
+        public string Icici_AccountNumber { get; set; }
+        public string Location { get; set; }
+        public string Icici_AccountBalance { get; set; }
+    }
+}
---
+++ CustomAttributesTest.cs
@@ -0,0 +1,57 @@
+﻿/*2)	Define a SoftwareAttribute which is having its base class as “Attribute” with following private members:
+	String ProjectName.
+	String Description.
+	String ClientName.
+	String StartedDate.
+	String EndDate.
+a)	Write properties for all these members
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Project8
+{
+    public class CustomAttributesTest
+    {
+        static void Main()
+        {
+            
+            var types = from t in Assembly.GetExecutingAssembly().GetTypes()
+                        where t.GetCustomAttributes<ClientAttribute>().Count() > 0
+                        select t;
+                
+            foreach(var  type in types)
+            {
+                //Name Of the class
+                Console.WriteLine(type.Name);
+                //Getting Properties List 
+                foreach (var t in type.GetProperties())
+                {
+                    Console.WriteLine("These  are properties present in the classs : {0}",t.Name);
+                }
+            }
+        }
+    }
+     [AttributeUsage(AttributeTargets.Class)]
+     /*CustomAttribute*/
+    public class ClientAttribute : Attribute
+    {
+        private string ProjectName { get; set; }
+        private string Descripition { get; set; }
+    }
+    
+    [Client]
+    public class SoftwareAttributes
+    {
+       
+        public string ProjectName { get; set; }
+        public string Descripition { get; set; }
+        public string ClientName { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+    }
+}
